01d4cc4d7248ce560ef4506f4b416196f30f8443
ThanhThuy21092001 <thuyb1906406@student.ctu.edu.vn>
2023-02-06 21:03:22 +0700

    video 9

---
+++ src/configs/connectDB.js
@@ -0,0 +1,14 @@
+// get the client
+import { resolveShowConfigPath } from '@babel/core/lib/config/files/configuration';
+import mysql from 'mysql2';
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'nodejsbasic'
+});
+
+
+
+export default connection;
--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,8 +1,31 @@
+import connection from "../configs/connectDB";
 
 let getHomepage = (req, res) => {
 
-    //login
-    return res.render('test/index.ejs')
+    //logic
+    // simple query
+    let data = [];
+    connection.query(
+        'SELECT * FROM `users` ',
+        function (err, results, fields) {
+            console.log('>>> check mysql')
+            console.log(results); // results contains rows returned by server
+            results.map((row) => {
+                data.push({
+                    id: row.id,
+                    email: row.email,
+                    address: row.address,
+                    firstName: row.firstName,
+                    lastName: row.lastName
+                })
+            });
+
+            return res.render('test/index.ejs', { dataUser: JSON.stringify(data) })
+
+        }
+
+    );
+    //console.log('>>> check data', typeof (data), JSON.stringify(data));
 }
 
 module.exports = {
